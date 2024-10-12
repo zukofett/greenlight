c65712aa78b7ed40b299c1086e2f52f54dbad768
idan.baker <idan.baker@infinitylabs.co.il>
2024-10-12 16:29:45 +0300

    initial commit

---
+++ cmd/api/movies.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"time"
+
+	"github.com/zukofett/greenlight/internal/data"
+)
+
+func (app *application) createMovieHandler(w http.ResponseWriter, r *http.Request) {
+    fmt.Fprintln(w, "creating movie")
+}
+func (app *application) showMovieHandler(w http.ResponseWriter, r *http.Request) {
+    id, err := app.readIDParam(r)
+    if err != nil {
+        app.notFoundResponse(w, r)
+        return
+    }
+
+    movie := data.Movie{
+        ID:        id,
+        CreatedAt: time.Now(),
+        Title:     "Casablanca",
+        Runtime:   102,
+        Genres:    []string{"drama", "romance", "war"},
+        Version:   1,
+    }
+
+    err = app.writeJSON(w, http.StatusOK, envelope{"movie":movie}, nil)
+    if err != nil {
+        app.serverErrorResponse(w, r, err)
+    }
+}
---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/zukofett/greenlight
+
+go 1.23.0
+
+require github.com/julienschmidt/httprouter v1.3.0 // indirect
