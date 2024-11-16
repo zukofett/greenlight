678457dd25169c0900f1a4d37a07759a100dd421
idan.baker <idan.baker@infinitylabs.co.il>
2024-11-16 17:54:14 +0200

    add sql migrations

---
+++ migrations/000002_add_movies_check_constraints.down.sql
@@ -0,0 +1,6 @@
+ALTER TABLE movies DROP CONSTRAINT IF EXISTS movie_runtime_check;
+
+ALTER TABLE movies DROP CONSTRAINT IF EXISTS movie_year_check;
+
+ALTER TABLE movies DROP CONSTRAINT IF EXISTS genres_length_check;
+
