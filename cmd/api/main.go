aafe18fc23e6f057bdc2eaf2d64b85ac2c6aced6
idan.baker <idan.baker@infinitylabs.co.il>
2024-11-16 11:22:44 +0200

    add crud operations

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,12 +1,17 @@
 package main
 
 import (
+	"context"
+	"database/sql"
 	"flag"
 	"fmt"
 	"log/slog"
 	"net/http"
 	"os"
 	"time"
+
+	_ "github.com/lib/pq"
+	"github.com/zukofett/greenlight/internal/data"
 )
 
 const version = "1.0.0"
@@ -14,11 +19,18 @@ const version = "1.0.0"
 type config struct {
     port int
     env  string
+    db struct {
+        dsn          string
+        maxOpenConns int
+        maxIdleConns int
+        maxIdleTime  time.Duration
+    }
 }
 
 type application struct {
     config config
     logger *slog.Logger
+    models data.Models
 }
 
 
@@ -27,13 +39,30 @@ func main() {
     
     flag.IntVar(&cfg.port, "port", 4000, "API server port")
     flag.StringVar(&cfg.env, "env", "development", "Environment (development|staging|production)")
+    flag.StringVar(&cfg.db.dsn, "db-dsn", os.Getenv("GREENLIGHT_DB_DSN"), "PostgreSQL DSN")
+
+    flag.IntVar(&cfg.db.maxOpenConns, "db-max-open-conns", 25, "PostgreSQL max open connections")
+    flag.IntVar(&cfg.db.maxIdleConns, "db-max-idle-conns", 25, "PostgreSQL max idle connections")
+    flag.DurationVar(&cfg.db.maxIdleTime, "db-max-idle-time", 15*time.Minute, "PostgreSQL max connectio idle time")
+
     flag.Parse()
 
     logger := slog.New(slog.NewTextHandler(os.Stdout, nil))
 
+    db, err := openDB(cfg)
+    if err != nil {
+        logger.Error(err.Error())
+        os.Exit(1)
+    }
+    defer db.Close()
+
+    logger.Info("database connection pool established")
+
+
     app := &application{
         config: cfg,
         logger: logger,
+        models: data.NewModels(db),
     }
 
 
@@ -48,7 +77,30 @@ func main() {
 
     logger.Info("starting server", "addr", srv.Addr, "env", cfg.env)
 
-    err := srv.ListenAndServe()
+    err = srv.ListenAndServe()
     logger.Error(err.Error())
     os.Exit(1)
+}
+
+func openDB(cfg config) (*sql.DB, error) {
+    db, err  := sql.Open("postgres", cfg.db.dsn)
+    if err != nil {
+        return nil, err
+    }
+
+    db.SetMaxOpenConns(cfg.db.maxOpenConns)
+    db.SetMaxIdleConns(cfg.db.maxIdleConns)
+    db.SetConnMaxIdleTime(cfg.db.maxIdleTime)
+
+
+    ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+    defer cancel()
+
+    err = db.PingContext(ctx)
+    if err != nil {
+        db.Close()
+        return nil, err
+    }
+
+    return db, nil
 }
--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module github.com/zukofett/greenlight
 
 go 1.23.0
 
-require github.com/julienschmidt/httprouter v1.3.0 // indirect
+require (
+	github.com/julienschmidt/httprouter v1.3.0 // indirect
+	github.com/lib/pq v1.10.9 // indirect
+)
